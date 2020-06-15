0b7baf8e332750ee277d05f5fa3984e66aaeb481
chapmanCC <chapboy1989@gmail.com>
2020-06-16 00:05:16 +0800

    added routes

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,38 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
+
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    // * index page
+
+});
+
+$app->get('/detail', function (Request $request, Response $response, array $args) {
+    // * detail page
+
+});
+
+$app->get('/edit', function (Request $request, Response $response, array $args) {
+    // * edit page
+
+});
+
+$app->get('/new', function (Request $request, Response $response, array $args) {
+    // * new page
+
+});
+
+$app->get('/password', function (Request $request, Response $response, array $args) {
+    // * password page
+
 });
