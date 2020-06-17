b9436dc9aeed72ef9fb419d51c0e023bc8968a68
chapmanCC <chapboy1989@gmail.com>
2020-06-17 12:52:35 +0800

    use switch statment for route

--- src/routes.php
+++ src/routes.php
@@ -22,21 +22,23 @@ $app->map(['GET', 'POST'], '/blog/{slug}', function (Request $request, Response 
     $slug = filter_var($args['slug'], FILTER_SANITIZE_STRING);
     $path = htmlspecialchars($request->getUri()->getPath());
 
-    // TODO: use switch()
-    if ($request->isPost()) {
-        $postBody = $request->getParsedBody();
-        $mailer = new Mailer($postBody);
-        $mailer->addPostID($id);
+    switch ($request->getMethod()) {
+        case 'POST':
+            $postBody = $request->getParsedBody();
+            $mailer = new Mailer($postBody);
+            $mailer->addPostID($id);
 
-        $comment->createComment($mailer->getPostData());
+            $comment->createComment($mailer->getPostData());
+            break;
+
+        default:
+            $blog = $post->getPostBySlug($slug);
+            return $this->view->render($response, 'detail.twig', [
+                'post' => $blog,
+                'comments' => $comment->getCommentsByCourseId($blog['id']),
+                'path' => $path
+            ]);
     }
-
-    $blog = $post->getPostBySlug($slug);
-    return $this->view->render($response, 'detail.twig', [
-        'post' => $blog,
-        'comments' => $comment->getCommentsByCourseId($blog['id']),
-        'path' => $path
-    ]);
 });
 
 
@@ -48,57 +50,64 @@ $app->map(['GET', 'PUT', 'DELETE'], '/edit', function (Request $request, Respons
     $uri = $request->getUri();
     $path = htmlspecialchars($uri->getPath());
 
-    if ($request->isPut()) {
-        $postBody = $request->getParsedBody();
-        $mailer = new Mailer($postBody);
-        $mailer->addPostID($id);
-        $mailer->slugifyTitle();
+    switch ($request->getMethod()) {
+        case 'PUT':
+            $postBody = $request->getParsedBody();
+            $mailer = new Mailer($postBody);
+            $mailer->addPostID($id);
+            $mailer->slugifyTitle();
 
-        if ($mailer->lookForTags())
-            $mailer->stripTagsFromBody()->addTags();
+            if ($mailer->lookForTags())
+                $mailer->stripTagsFromBody()->addTags();
 
-        $upPost = $post->updatePost($mailer->getPostData());
-        return $response->withRedirect("/blog/$upPost[slug]");
-    } elseif ($request->isDelete()) {
-        $post->deletePost($id);
-        return $response->withRedirect('/');
+            $upPost = $post->updatePost($mailer->getPostData());
+            return $response->withRedirect("/blog/$upPost[slug]");
+        case 'DELETE':
+            $post->deletePost($id);
+            return $response->withRedirect('/');
+
+        default:
+            return $this->view->render($response, 'edit.twig', [
+                'post' => $post->getPost($id),
+                'path' => $path
+            ]);
+            break;
     }
-
-    return $this->view->render($response, 'edit.twig', [
-        'post' => $post->getPost($id),
-        'path' => $path
-    ]);
 });
 
 $app->map(['GET', 'POST'], '/new', function (Request $request, Response $response, array $args) {
     $uri = $request->getUri();
     $path = htmlspecialchars($uri->getPath());
 
-    if ($request->isPost()) {
-        $post = new \Model\Post();
+    switch ($request->getMethod()) {
+        case 'POST':
+            $post = new \Model\Post();
 
-        $postBody = $request->getParsedBody();
-        $mailer = new Mailer($postBody);
-        $mailer->slugifyTitle();
+            $postBody = $request->getParsedBody();
+            $mailer = new Mailer($postBody);
+            $mailer->slugifyTitle();
 
 
-        if ($mailer->lookForTags())
-            $mailer->stripTagsFromBody()->addTags();
+            if ($mailer->lookForTags())
+                $mailer->stripTagsFromBody()->addTags();
 
-        $newPost = $post->createPost($mailer->getPostData());
+            $newPost = $post->createPost($mailer->getPostData());
 
-        if ($newPost)
-            return $response->withRedirect("/blog/$newPost[slug]");
-        else
+            if ($newPost)
+                return $response->withRedirect("/blog/$newPost[slug]");
+            else
+                return $this->view->render($response, 'new.twig', [
+                    'path' => $path,
+                    'post' => $newPost
+                ]);
+            break;
+
+        default:
             return $this->view->render($response, 'new.twig', [
-                'path' => $path,
-                'post' => $newPost
+                'path' => $path
             ]);
+            break;
     }
-
-    return $this->view->render($response, 'new.twig', [
-        'path' => $path
-    ]);
 });
 
 // ! unused material
